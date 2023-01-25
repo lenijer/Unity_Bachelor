fce5c428cb102443c191e50b0c14c2a7c9a5779d
lenijer <70570095+lenijer@users.noreply.github.com>
2023-01-25 12:26:40 +0100

    works

---
+++ VR_Battery_Lab/Assets/Script/Animate_HandOnInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Animate_HandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimAction;
+    public InputActionProperty gripAnimAction;
+
+    public Animator HandAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggervalue = pinchAnimAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Trigger", triggervalue);
+
+        float gripvalue = gripAnimAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Grip", gripvalue);
+
+        Debug.Log(triggervalue);
+    }
+}
