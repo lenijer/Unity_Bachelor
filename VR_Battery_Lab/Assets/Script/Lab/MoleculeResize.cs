56a3f730980ba4290eaabf9646994a89a940008a
lenijer <70570095+lenijer@users.noreply.github.com>
2023-04-24 12:01:09 +0200

    before ttravel

---
+++ VR_Battery_Lab/Assets/Script/Lab/MoleculeResize.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoleculeResize : MonoBehaviour
+{
+    public GameObject Corner_1;
+    public GameObject Corner_2;
+    public GameObject Corner_3;
+    public GameObject Corner_4;
+    public GameObject Corner_5;
+    public GameObject Corner_6;
+    public GameObject Corner_7;
+    public GameObject Corner_8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
